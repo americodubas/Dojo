2f513d56e0321f8ee9040326ccaaac54dbe33f08
Américo Dubas <americo.dubas@gmail.com>
2018-02-21 22:48:31 -0300

    first commit dojo check

--- src/main/kotlin/ReadCheck.kt
+++ src/main/kotlin/ReadCheck.kt
@@ -1,60 +1,94 @@
+/**
+ * Dado um cheque com o seu valor por extenso,
+ * desenvolva um programa que retorne o seu valor numérico.
+ * Por exemplo
+ * se informarmos "dois mil quinhentos e vinte e três reais e dezoito centavos",
+ * o seu programa deve retornar "2523,18"
+ *
+ */
 
-private val unity = mapOf(Pair ("um",1),Pair ("dois",2),Pair ("tres",3),Pair ("quatro",4)
-        , Pair ("cinco",5), Pair ("seis",6),Pair ("sete",7),Pair ("oito",8), Pair ("nove",9))
+//This map contains all numbers names and values to a maximum of XXX
+private val numberValue = mapOf(
+        "um" to 1,
+        "dois" to 2,
+        "tres" to 3,
+        "quatro" to 4,
+        "cinco" to 5,
+        "seis" to 6,
+        "sete" to 7,
+        "oito" to 8,
+        "nove" to 9,
+        "dez" to 10,
+        "onze" to 11,
+        "doze" to 12,
+        "treze" to 13,
+        "quatorze" to 14,
+        "quinze" to 15,
+        "dezesseis" to 16,
+        "dezessete" to 17,
+        "dezoito" to 18,
+        "dezenove" to 19,
+        "vinte" to 20,
+        "trinta" to 30,
+        "quarenta" to 40,
+        "cinquenta" to 50,
+        "sessenta" to 60,
+        "setenta" to 70,
+        "oitenta" to 80,
+        "noventa" to 90,
+        "cem" to  100,
+        "cento" to  100,
+        "duzentos" to  200,
+        "trezentos" to  300,
+        "quatrocentos" to  400,
+        "quinhentos" to  500,
+        "seiscentos" to  600,
+        "seteentos" to  700,
+        "oitoentos" to  800,
+        "novecentos" to  900)
 
-private val dezena = mapOf(Pair ("dez",10),
-        Pair ("onze",11),
-        Pair ("doze",12),
-        Pair ("treze",13),
-        Pair ("quatorze",14),
-        Pair ("quinze",15),
-        Pair ("dezesseis",16),
-        Pair ("dezessete",17),
-        Pair ("dezoito",18),
-        Pair ("dezenove",19),
-        Pair ("vinte",20),Pair ("trinta",30),
-        Pair ("quarenta",40),Pair ("cinquenta",50),Pair ("sessenta",60),Pair ("setenta",70),
-        Pair ("oitenta",80),Pair ("noventa",90))
+//This map contains the three part delimiters and it's multiplier value
+//To receive bigger numbers just add a new delimiter
+private val delimiterMultiplier = mapOf(
+        " milhão " to 1000000.0f,
+        " mil " to 1000.0f,
+        " reais" to 1.0f,
+        " real" to 1.0f,
+        " centavos" to 0.01f,
+        " centavo" to 0.01f
+)
 
-private val hundred = mapOf(Pair("cem", 100),
-        Pair("cento", 100),
-        Pair("duzentos", 200),
-        Pair("trezentos", 300),
-        Pair("quatrocentos", 400),
-        Pair("quinhentos", 500),
-        Pair("seissentos", 600),
-        Pair("seteentos", 700),
-        Pair("oitoentos", 800),
-        Pair("novecentos", 900))
+private var remain: String = ""
+private var total = 0.0f
 
-private val delimiter = arrayOf(" mil", " real", " reais")
-private val delimiterCents = arrayOf(" centavo", " centavos")
+//Receives a writen check value and convert to it's value
+fun convertCheckToNumber(check: String): Float {
+    total = 0.0f
+    remain = check
+    delimiterMultiplier.forEach{ k, v -> splitByDelimiterAndMultiplyByIt(k, v) }
+    return total
+}
 
-fun convertDezenaToNumber (fullCheckName: String ) : Int {
-    return  when (dezena.contains(fullCheckName.toLowerCase())) {
-        true -> dezena.getValue(fullCheckName.toLowerCase())
-        false -> 0
+//This method splits the value according to the delimiters
+//The result will be a three part number
+//Multiply the number be the delimiter value
+fun splitByDelimiterAndMultiplyByIt(key: String, value: Float) {
+    if (remain.isEmpty() || !remain.contains(key)) {
+        return
     }
+    val split = remain.split(key)
+    remain = split[1]
+    total += convertPartToNumber(split[0]) * value
 }
 
-fun convertUnitToNumber (fullCheckName: String ) : Int {
-    return  when (unity.contains(fullCheckName.toLowerCase())) {
-            true -> unity.getValue(fullCheckName.toLowerCase())
-            false -> 0
-        }
-}
-
-fun convertHundredToNumber (fullCheckName: String ) : Int {
-    return  when (hundred.contains(fullCheckName.toLowerCase())) {
-        true -> hundred.getValue(fullCheckName.toLowerCase())
-        false -> 0
+//This method should receive a three part writen number and convert to it's value
+fun convertPartToNumber(part: String): Int {
+    val split = splitTheAndSeparator(part)
+    var sum = 0
+    split.forEach {
+        if (it.trim().isNotEmpty()) sum += numberValue.getValue(it.trim())
     }
+    return sum
 }
 
-fun splitValueBlock (fullCheckName : String) : List<String> {
-    return fullCheckName.split(" e ")
-}
-
-fun returnThousandValue (fullCheckName: String) : String {
-    var values = fullCheckName.split(delimiterReal)
-}
+fun splitTheAndSeparator(fullCheckName : String) = fullCheckName.split(" e ")
--- src/test/kotlin/ReadCheckKtTest.kt
+++ src/test/kotlin/ReadCheckKtTest.kt
@@ -5,33 +5,20 @@ import org.junit.Assert.*
 class ReadCheckKtTest {
 
     @Test
-    fun convertUnitToNumber() {
-        assertEquals(1, convertUnitToNumber("um"))
-        assertEquals(2, convertUnitToNumber("dois"))
-    }
-
-    @Test
-    fun convertDezenaToNumber() {
-        assertEquals(10, convertDezenaToNumber("dez"))
-        assertEquals(20, convertDezenaToNumber("vinte"))
-    }
-
-    @Test
-    fun convertHundredToNumber() {
-        assertEquals(100, convertHundredToNumber("cem"))
-        assertEquals(100, convertHundredToNumber("cento"))
-        assertEquals(600, convertHundredToNumber("seissentos"))
-    }
-
-    @Test
     fun splitValueBlock() {
-        assertEquals(listOf("cento", "dez"), splitValueBlock("cento e dez"))
-        assertEquals(listOf("trezentos", "cinquenta", "dois"), splitValueBlock("trezentos e cinquenta e dois"))
+        assertEquals(listOf("cento", "dez"), splitTheAndSeparator("cento e dez"))
+        assertEquals(listOf("trezentos", "cinquenta", "dois"), splitTheAndSeparator("trezentos e cinquenta e dois"))
     }
 
     @Test
-    fun returnThousandValue() {
-        assertEquals(true, returnThousandValue("trezentos e vinte e cinco mil"))
-        assertEquals(false, returnThousandValue("trezentos e cinquenta e dois"))
+    fun convertFullCheckToNumber() {
+        assertEquals(2000.0f, convertCheckToNumber("dois mil reais"))
+        assertEquals(52000.0f, convertCheckToNumber("cinquenta e dois mil reais"))
+        assertEquals(52641.0f, convertCheckToNumber("cinquenta e dois mil seiscentos e quarenta e um reais"))
+        assertEquals(600.0f, convertCheckToNumber("seiscentos reais"))
+        assertEquals(1000000.0f, convertCheckToNumber("um milhão reais"))
+        assertEquals(1.0f, convertCheckToNumber("um real"))
+        assertEquals(1.25f, convertCheckToNumber("um real e vinte e cinco centavos"))
+        assertEquals(1234567.89f, convertCheckToNumber("um milhão duzentos e trinta e quatro mil quinhentos e sessenta e sete reais e oitenta e nove centavos"))
     }
 }
